d24423247c17b85d39d8cc20f00b75d590ece258
ghkdwlscks <ghkdwlscks@gmail.com>
2022-10-14 08:28:51 +0900

    Update windows_setup.bat

--- windows_setup.bat
+++ windows_setup.bat
@@ -4,5 +4,5 @@ REM This is the batch file used for generating Windows installer.
 REM NewsAnalyzer.zip includes build (PyInstaller output directory), config, data, fasttext and format directories.
 mkdir "C:\Program Files\NewsAnalyzer"
 bandizip x -y -o:"C:\Program Files\NewsAnalyzer" "NewsAnalyzer.zip"
-bitsadmin /transfer "Downloading pretrained fastText model..." "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ko.300.bin.gz" "C:\Program Files\NewsAnalyzer\fasttext\pretrained_model.bin.gz"
 mklink "%userprofile%\desktop\NewsAnalyzer.exe" "C:\Program Files\NewsAnalyzer\build\NewsAnalyzer.exe"
+bitsadmin /transfer "Downloading pretrained fastText model..." "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ko.300.bin.gz" "C:\Program Files\NewsAnalyzer\fasttext\pretrained_model.bin.gz"
